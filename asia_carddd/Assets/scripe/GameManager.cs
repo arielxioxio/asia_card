95bd06ab760c50e57ec7c4b4f6f5c78acb16ae4b
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2020-11-03 12:04:43 +0800

    旋轉縮放控制器

--- asia_carddd/Assets/scripe/GameManager.cs
+++ asia_carddd/Assets/scripe/GameManager.cs
@@ -2,10 +2,36 @@
 
 public class GameManager : MonoBehaviour
 {
+    [Header("女" )]
    public Transform women;
+    [Header("男")]
     public Transform man;
-     
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+   [Header("旋轉速度"),Range(0.1f,20f)]
+    public float turn = 1.5f;
+    [Header("縮放"), Range(0f, 5f)]
+    public float size = 0.3f;
 
-       
-    
+
+    private void Start()
+
+    {
+        print("開始事件執行中");
+
+ 
+    }
+    private void Update()
+    {
+        print("更新事件");
+            print(joystick.Vertical);
+        women.Rotate(0, joystick.Horizontal*turn, 0);
+        man.Rotate(0, joystick.Horizontal * turn, 0);
+
+        women.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size ;    }
+
+
+
+
+
 }
